a55d00a0e7dc09fe39c31e07c8ae51b42a2333b7
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-12-02 14:12:32 -0200

    adding more tests for the tsuru client

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,8 +11,10 @@ class MorfeuTsuruClientTestCase(unittest.TestCase):
     def tearDown(self):
         self.tsuru_client = None
 
+    ### LIST APPS ###
+
     @httpretty.activate
-    def test_client_list_apps_with_success(self):
+    def test_client_list_web_apps_with_success(self):
 
         expected_response = '[{"ip":"10.10.10.10","name":"app1","units":[{"ID":"app1/0","Status":"started", "ProcessName": "web"}]}]'
         httpretty.register_uri(httpretty.GET, TsuruClientUrls.list_apps_url(),
@@ -21,6 +23,27 @@ class MorfeuTsuruClientTestCase(unittest.TestCase):
                        status=200)
         self.assertEqual(self.tsuru_client.list_apps(), [{u'app1': [u'app1/0']}])
 
+    @httpretty.activate
+    def test_client_list_no_web_apps(self):
 
+        expected_response = '[{"ip":"10.10.10.10","name":"app1","units":[{"ID":"app1/0","Status":"started", "ProcessName": "worker"}]}]'
+        httpretty.register_uri(httpretty.GET, TsuruClientUrls.list_apps_url(),
+                       body=expected_response,
+                       content_type="application/json",
+                       status=200)
+        self.assertEqual(self.tsuru_client.list_apps(), [])
+
+    @httpretty.activate
+    def test_client_list_with_failure(self):
+
+        expected_response = '[]'
+        httpretty.register_uri(httpretty.GET, TsuruClientUrls.list_apps_url(),
+                       body=expected_response,
+                       content_type="application/json",
+                       status=500)
+        self.assertEqual(self.tsuru_client.list_apps(), [])
+
+
+    ### END LIST APPS ###
 if __name__ == '__main__':
     unittest.main()
