ecef185b3f5463730ac5959cf8b3f55bbb9c06ff
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-04-07 20:23:57 -0300

    Use Graylog to check if apps have access

--- morfeu/tsuru/app.py
+++ morfeu/tsuru/app.py
@@ -1,8 +1,8 @@
-import requests
 import logging
 
 from morfeu.tsuru.client import TsuruClient
-from morfeu.settings import TIME_RANGE_IN_HOURS, ESEARCH_HOST, TIMEOUT
+from morfeu.settings import TIME_RANGE_IN_HOURS, TIMEOUT
+from morfeu.graylog import GraylogClient
 
 LOG = logging.getLogger(__name__)
 
@@ -40,29 +40,5 @@ class TsuruApp(object):
             tsuru_client.stop_app(app_name=self.name)
 
     def should_go_to_bed(self, time_range=TIME_RANGE_IN_HOURS):
-        payload = {
-            "query": {
-                "filtered": {
-                    "filter": {
-                        "and": [
-                            {"range": {"@timestamp": {"gt": "now-{}h".format(time_range)}}},
-                            {"term": {"app.raw": self.name}}
-                        ]
-                    }
-                }
-            }
-        }
-
-        url = "http://{0}/.measure-tsuru-*/response_time/_search".format(ESEARCH_HOST)
-        req = requests.post(url, json=payload, timeout=self.timeout)
-
-        if req.status_code != 200:
-            LOG.info("Error getting app {} metrics data".format(self.name))
-            return False
-
-        response = req.json()
-        hits = response.get("hits", {})
-        hits_ = hits.get("hits", [])
-        LOG.info("Getting hits for \"{}\"".format(self.name))
-
-        return not hits_
+        results = GraylogClient(self).search()
+        return len(results) == 0
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,11 @@ import json
 import httpretty
 import unittest
 import re
-
+from mock import patch
 from morfeu.tsuru.app import TsuruApp
 from morfeu.tsuru.client import TsuruClientUrls
 from morfeu.settings import ESEARCH_HOST, TSURU_APP_PROXY_URL
+from morfeu.graylog import GraylogClient
 
 
 class AppTestCase(unittest.TestCase):
@@ -24,38 +25,15 @@ class AppTestCase(unittest.TestCase):
         httpretty.register_uri(httpretty.GET, TsuruClientUrls.get_app_url(app),
                                body=expected_response, content_type="application/json", status=200)
 
-    @httpretty.activate
-    def test_should_go_to_bed_when_app_isnt_new_and_has_no_hits(self):
-        url = "http://{0}/.measure-tsuru-*/response_time/_search".format(ESEARCH_HOST)
-        httpretty.register_uri(httpretty.POST, url,
-                               body="{}", content_type="application/json", status=200)
-
-        self.mock_app("myapp")
-
+    @patch.object(GraylogClient, "search")
+    def test_should_go_to_bed_when_app_didnt_have_access(self, search):
+        search.return_value = []
         app = TsuruApp(name="myapp")
         self.assertTrue(app.should_go_to_bed())
 
-    @httpretty.activate
-    def test_should_not_go_to_bed_when_app_is_new_and_has_no_hits(self):
-        url = "http://{0}/.measure-tsuru-*/response_time/_search".format(ESEARCH_HOST)
-        httpretty.register_uri(httpretty.POST, url,
-                               body="{}", content_type="application/json", status=200)
-
-        self.mock_app("myapp")
-
-        app = TsuruApp(name="myapp")
-        self.assertTrue(app.should_go_to_bed())
-
-    @httpretty.activate
-    def test_should_not_go_to_bed_when_app_isnt_new_and_has_hits(self):
-        url = "http://{0}/.measure-tsuru-*/response_time/_search".format(ESEARCH_HOST)
-        httpretty.register_uri(httpretty.POST, url,
-                               body='{"hits": {"hits": [1, 2, 3]}}',
-                               content_type="application/json",
-                               status=200)
-
-        self.mock_app("myapp")
-
+    @patch.object(GraylogClient, "search")
+    def test_should_not_go_to_bed_when_app_had_access(self, search):
+        search.return_value = ["message"]
         app = TsuruApp(name="myapp")
         self.assertFalse(app.should_go_to_bed())
 
